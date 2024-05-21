25c23a4ff0a1b42d0a7dcd930a342e858114ac42
soheil-01 <soheil.rasekh.01@gmail.com>
2024-05-22 00:27:14 +0330

    feat: parse signature

--- src/ecc/signature.zig
+++ src/ecc/signature.zig
@@ -5,6 +5,8 @@ const Signature = @This();
 r: u256,
 s: u256,
 
+pub const Error = error{BadSignature};
+
 pub const der_encoded_max_length = 72;
 
 pub fn init(r: u256, s: u256) Signature {
@@ -15,9 +17,8 @@ pub fn toString(self: Signature, allocator: std.mem.Allocator) ![]u8 {
     return std.fmt.allocPrint(allocator, "Signature({x},{x})", .{ self.r, self.s });
 }
 
-pub fn toDer(self: Signature) []u8 {
-    var buf: [der_encoded_max_length]u8 = undefined;
-    var fb = std.io.fixedBufferStream(&buf);
+pub fn toDer(self: Signature, buf: *[der_encoded_max_length]u8) []u8 {
+    var fb = std.io.fixedBufferStream(buf);
     const w = fb.writer();
 
     var r_bytes: [32]u8 = undefined;
@@ -45,4 +46,74 @@ pub fn toDer(self: Signature) []u8 {
     w.writeAll(&s_bytes) catch unreachable;
 
     return fb.getWritten();
+}
+
+pub fn parse(der: []const u8) !Signature {
+    var fb = std.io.fixedBufferStream(der);
+    const reader = fb.reader();
+
+    const compound = reader.readByte() catch return Error.BadSignature;
+    if (compound != 0x30) {
+        std.debug.print("compound: {d}\n", .{compound});
+        return Error.BadSignature;
+    }
+
+    const sig_len = reader.readByte() catch return Error.BadSignature;
+    if (sig_len + 2 != der.len) {
+        std.debug.print("sig_len: {d}, der.len: {d}\n", .{ sig_len, der.len });
+        return Error.BadSignature;
+    }
+
+    const r = try parseDerInt(reader);
+    const s = try parseDerInt(reader);
+
+    if (fb.getPos() catch unreachable != der.len) {
+        return Error.BadSignature;
+    }
+
+    return init(r, s);
+}
+
+fn parseDerInt(reader: anytype) !u256 {
+    const marker = reader.readByte() catch return Error.BadSignature;
+    if (marker != 0x02) {
+        return Error.BadSignature;
+    }
+
+    var buf: [32]u8 = undefined;
+    var len = reader.readByte() catch return Error.BadSignature;
+    if (len == 0 or len > buf.len + 1) {
+        return Error.BadSignature;
+    }
+
+    if (len == buf.len + 1) {
+        if ((reader.readByte() catch return Error.BadSignature) != 0) {
+            return Error.BadSignature;
+        }
+        len -= 1;
+    }
+
+    reader.readNoEof(&buf) catch return Error.BadSignature;
+
+    return std.mem.readInt(u256, &buf, .big);
+}
+
+const testing = std.testing;
+
+test "Signature: der" {
+    const rand = std.crypto.random;
+
+    const test_cases = .{ .{ .r = 1, .s = 2 }, .{ .r = rand.int(u256), .s = rand.int(u256) }, .{ .r = rand.int(u256), .s = rand.int(u256) } };
+
+    inline for (test_cases) |case| {
+        const sig = Signature.init(case.r, case.s);
+
+        var buf: [72]u8 = undefined;
+        const der = sig.toDer(&buf);
+
+        const sig2 = try parse(der);
+
+        try testing.expectEqual(sig.r, sig2.r);
+        try testing.expectEqual(sig.s, sig2.s);
+    }
 }
