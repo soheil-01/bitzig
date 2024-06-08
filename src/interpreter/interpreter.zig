e1c2045b4a7f9be8210f270280134a15c68cd786
soheil <soheil.rasekh.01@gmail.com>
2024-06-08 12:41:45 +0330

    fix(interpreter): opCheckSig segmentation fault

--- src/interpreter/interpreter.zig
+++ src/interpreter/interpreter.zig
@@ -103,7 +103,11 @@ fn opCheckSig(self: Interpreter, options: Options) Error!bool {
     }
 
     const sec_pubkey = options.stack.pop();
+    defer self.allocator.free(sec_pubkey);
+
     const last = options.stack.pop();
+    defer self.allocator.free(last);
+
     const der_signature = last[0 .. last.len - 1];
 
     const point = try S256Point.fromSec(sec_pubkey);
@@ -234,10 +238,7 @@ test "Interpreter: opCheckSig" {
     const z = 0x7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d;
 
     const sec = try utils.hexToBytes(testing_alloc, "04887387e452b8eacc4acfde10d9aaf7f6d9a0f975aabb10d006e4da568744d06c61de6d95231cd89026e286df3b6ae4a894a3378e393e93a0f45b666329a0ae34");
-    defer testing_alloc.free(sec);
-
     const sig = try utils.hexToBytes(testing_alloc, "3045022000eff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c022100c7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab601");
-    defer testing_alloc.free(sig);
 
     try stack.appendSlice(&.{ sig, sec });
 
